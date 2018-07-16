eaa306a93ad5a3e6bc31f1cd702d8a1522972ede
jypark97 <jypark97@berkeley.edu>
2018-07-16 11:04:38 -0700

    adding example for mui package

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends React.Component {
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+    return (
+      <MuiThemeProvider>
+        <AppBar
+          title="Title of the doc to edit"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+      </MuiThemeProvider>
+    );
   }
 }
