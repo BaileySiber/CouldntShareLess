7e9f4114481f3becf2da18526a74e0d0459dcd4d
jypark97 <jypark97@berkeley.edu>
2018-07-16 11:43:09 -0700

    removing package-lock.json

--- src/doc.jsx
+++ src/doc.jsx
@@ -13,11 +13,28 @@ export default class Doc extends React.Component {
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
   }
 
+  // onItalicClick(e) {
+  //   e.preventDefault();
+  //   this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+  // }
+
+  // onUnderlineClick(e) {
+  //   e.preventDefault();
+  //   this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  // }
+
+//   const styleMap = {
+//   'STRIKETHROUGH': {
+//     textDecoration: 'line-through',
+//   },
+// };
+
   render() {
     return (<div>
       <button onMouseDown={e => this.onBoldClick(e)}>BOLD</button>
       <Editor
         editorState={this.state.editorState}
+        // customStyleMap={styleMap}
         onChange={this.onChange}
       />
     </div>);
