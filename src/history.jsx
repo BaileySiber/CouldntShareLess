b588da9277d8a63962a8b9c3539db455fd1f4a60
smedlang <smedlang@gmail.com>
2018-07-20 14:45:29 -0700

    revision history

--- src/history.jsx
+++ src/history.jsx
@@ -1,14 +1,69 @@
 import React from 'react';
 import { Editor, EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js';
+import createStyles from 'draft-js-custom-styles';
 
 
+const customStyleMap = {
+  green: {
+    color: "#36db39"
+  },
+  blue: {
+    color:"#0019ff"
+  },
+  red: {
+    color:"#ff0000"
+  },
+  purple: {
+    color:"#a100ff"
+  },
+  orange: {
+    color:"#ff8c00"
+  },
+  black: {
+    color: '#000000',
+  },
+};
+
+
+const blockStyle = (block) => {
+  switch (block.getType()) {
+    case 'left':
+    return 'align-left';
+    case 'center':
+    return 'align-center';
+    case 'right':
+    return 'align-right';
+    case 'unordered-list-item':
+    return 'unordered';
+    case 'ordered-list-item':
+    return 'ordered';
+    default:
+    return null;
+  }
+}
+
+const { styles, customStyleFn, exporter } = createStyles(['font-size', 'color'], 'PREFIX', customStyleMap);
+
+// color
+const toggleColor = styles.color.toggle;
+const addColor = styles.color.add;
+const removeColor = styles.color.remove;
+const currentColor = styles.color.current;
+
+
+
+// fontSize
+const toggleFontSize = styles.fontSize.toggle;
+const addFontSize = styles.fontSize.add;
+const removeFontSize = styles.fontSize.remove;
+const currentFontSize = styles.fontSize.current;
 export default class History extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       lastEditTimeArray: [],
       contentArray: [],
-      oldState: ''
+      oldState: EditorState.createEmpty()
     }
   }
 
@@ -27,41 +82,82 @@ export default class History extends React.Component {
       return result.json()
     })
     .then((json) => {
-      console.log("content history ---------> "+json.content, "lastEditTime history ------> " + json.lastEditTime)
+      console.log(json.content)
       this.setState({
         contentArray: json.content,
-        lastEditTimeArray: json.lastEditTime
+        lastEditTimeArray: json.lastEditTime.map(time=>(new Date(time)).toUTCString())
       });
     }).catch(err => console.log(err))
   }
 
   onTime(index){
-    this.setState({oldState: EditorState.createWithContent(convertFromRaw(contentArray[index]))})
+    this.setState({
+      oldState: EditorState.createWithContent(convertFromRaw(this.state.contentArray[index]))
+    })
+  }
+  restore(){
+    fetch('http://localhost:1337/save', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        docId: this.props.docId,
+        content: convertToRaw(this.state.oldState.getCurrentContent()),
+      })
+    }).then(()=> {
+      this.props.navigate("doc", this.props.userId, this.props.docId)
+    })
+
   }
 
   render(){
     return (
       <div>
-
-        <div style={{
-          border: 'solid 1px',
-          borderRadius: '5px',
-          padding: '5px',
-          margin: '10px',
-          height: '50vh'
-        }}>
+        <button type="button"
+        style={{
+        width: "100px",
+        height: "50px",
+        border: 'none',
+        fontSize: "20px",
+        color: "white",
+        backgroundColor: "#c6b8ce",
+        float: "right"
+      }}
+      onClick={() => this.props.navigate("doc", this.props.userId, this.props.docId)}> Back</button>
+        <div style={{fontFamily:'Lucida, sans-serif', fontSize:"40px", padding: 10, color: "#a28baf"}}>
+        {this.props.username}'s History
+        </div>
+        <div style={{display: 'inlineBlock', height: "100px", border:"solid 8px", margin: 10, borderColor: "#a28baf", overflow: 'auto'}}>
         {this.state.lastEditTimeArray.map((timestamp, index) => <button onClick={()=> this.onTime(index)}> {timestamp} </button>)}
         </div>
-
-        <div style={{
-          border: 'solid 1px',
-          borderRadius: '5px',
-          padding: '5px',
-          margin: '10px',
-          height: '50vh'
-        }}>
-        // {EditorState.createWithContent(convertFromRaw(this.state.oldState))}
+        <div style={{fontFamily:'Lucida, sans-serif', fontSize:"40px", padding: 10, color: "#a28baf"}}>
+        Restore Version
         </div>
+        <div style={{display: 'inlineBlock', height: "100px", border:"solid 8px", margin: 10, borderColor: "#a28baf", overflow: 'auto'}}>
+          <Editor
+            customStyleFn={customStyleFn}
+            customStyleMap={customStyleMap}
+            editorState={this.state.oldState}
+            onChange={()=> {}}
+            blockStyleFn={blockStyle}
+            readOnly={true}/>
+        </div>
+
+        <div><button
+         type="button"
+         style={{
+         width: "100px",
+         height: "50px",
+         border: 'none',
+         fontSize: "20px",
+         color: "white",
+         backgroundColor: "#c6b8ce",
+         float: "right"
+       }}
+       onClick={()=> this.restore()}>
+         Restore
+       </button></div>
       </div>
     );
   }
